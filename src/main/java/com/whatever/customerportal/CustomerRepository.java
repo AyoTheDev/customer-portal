02c47449ff8acd534e66deca437cdfbbd60f4825
ayo <ayo.adedeji@crvsh.io>
2022-04-02 08:40:44 +0100

    ...

---
+++ src/main/java/com/whatever/customerportal/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.whatever.customerportal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
